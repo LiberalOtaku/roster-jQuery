52f67425ea54bdae64d6589081269bb309f61ff5
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-09 16:30:59 -0400

    Fixed slide animation and all buttons.

--- js/app.js
+++ js/app.js
@@ -3,21 +3,29 @@ $(document).foundation();
 var app = {
   // initialize app
   init: function(formSelector, listSelector) {
+    $.ajax({
+      url: 'https://xtern-roster.herokuapp.com/',
+      method: 'get',
+      success: function(sites) {
+        app.people = sites.resources.people + '/';
+        app.rivalries = sites.resources.rivalries + '/';
+      },
+    });
     this.form = $(formSelector);
     this.list = $(listSelector);
-    this.getLocalStorage();
+    // this.getLocalStorage();
     this.setupEventListeners();
-    this.refreshRoster();
+    // this.refreshRoster();
   },
 
-  getLocalStorage: function() {
-    var roster = JSON.parse(localStorage.getItem('roster'));
-    if (roster) {
-      $.each(roster, function(i, student) {
-        app.list.append(app.buildList(student.name, student.favorite));
-      });
-    }
-  },
+  // getLocalStorage: function() {
+  //   var roster = JSON.parse(localStorage.getItem('roster'));
+  //   if (roster) {
+  //     $.each(roster, function(i, student) {
+  //       app.list.append(app.buildList(student.name, student.favorite));
+  //     });
+  //   }
+  // },
 
   setupEventListeners: function() {
     this.form.submit(this.addStudent.bind(this));
@@ -26,8 +34,9 @@ var app = {
   },
 
   // now build the actual list entry for each new name
-  buildList: function(name, favorite) {
+  buildList: function(name, favorite, id) {
     var dl = $('<dl/>').attr({
+      "data-id": id,
       "class": (function() {
           if (favorite)
             return "favorite";
@@ -40,7 +49,7 @@ var app = {
     var dd = $('<dd/>');
     var ul = $('<ul/>').attr({"class": "button-group actions"});
     var editGroup = $('<li/>');
-    var moveGroup = $('<li/>');
+    // var moveGroup = $('<li/>');
 
     // create edit button
     var editLink = this.buildLink({
@@ -49,20 +58,37 @@ var app = {
       handler: function() {
         // create edit field
         if ((editLink.attr("class") === "update button tiny radius success") &&
-        ($('#input').val() !== '')) {
-          dt.text($('#input').val());
-          editLink.attr("class", "edit button tiny radius secondary");
-          app.saveList();
+        ($('[name="editName"]').val().length)) {
+          $.ajax({
+            url: app.people + dl.attr('data-id'),
+            method: 'patch',
+            contentType: "application/json",
+            data: JSON.stringify({
+              "person": {
+                "name": $('[name="editName"]').val(),
+                "promoted": (function() {
+                  return (dl.attr("class") === "favorite");
+                })(),
+              }
+            }),
+            success: function(data) {
+              dt.text($('[name="editName"]').val());
+              editLink.attr("class", "edit button tiny radius secondary");
+              editLink.children().first().attr("class", "fa fa-pencil fa-lg");
+            },
+          });
         }
         else {
           dt.html($('<input/>').attr({
-            id: "input",
+            name: "editName",
             type: "text",
             "class": "edit medium-6 columns",
-            placeholder: "Enter Student Name",
+            placeholder: "Enter Your Name",
+            required: true,
           }).val(dt.text()));
           dt.children().first().focus().select();
           editLink.attr("class", "update button tiny radius success");
+          editLink.children().first().attr("class", "fa fa-check fa-lg");
         }
       }
     });
@@ -72,9 +98,16 @@ var app = {
       contents: '<i class="fa fa-times fa-lg"></i>',
       class: "remove button tiny radius alert",
       handler: function() {
-        dl.remove();
-        app.refreshRoster();
-        app.saveList();
+        $.ajax({
+          url: app.people + dl.attr('data-id'),
+          method: 'delete',
+          success: function() {
+            app.list.slideUp();
+            dl.remove();
+            // app.refreshRoster();
+            app.list.slideDown();
+          },
+        });
       }
     });
 
@@ -83,60 +116,89 @@ var app = {
       contents: '<i class="fa fa-star fa-lg"></i>',
       class: "favorite button tiny radius",
       handler: function() {
-        // just switching the item background color for now
-        if (dl.attr("class") === "favorite")
-          dl.removeClass("favorite");
-        else dl.addClass("favorite");
-        app.saveList();
-      }
-    });
-
-    // create top button
-    var topLink = this.buildLink({
-      contents: '<i class="fa fa-arrow-circle-up fa-lg"></i>',
-      class: "top button tiny radius",
-      handler: function() {
-        // move item to the top
-        dl.insertBefore(dl.siblings().first());
-        app.refreshRoster();
-        app.saveList();
-      }
-    });
-
-    // create up button
-    var upLink = this.buildLink({
-      contents: '<i class="fa fa-arrow-up fa-lg"></i>',
-      class: "up button tiny radius",
-      handler: function() {
-        // move item up one space
-        var prevDL = dl.prev();
-        if (prevDL.length) {
-          dl.insertBefore(prevDL);
-          app.refreshRoster();
-          app.saveList();
+        if (editLink.attr("class") === "edit button tiny radius secondary") {
+          if (dl.attr("class") === "favorite") {
+            $.ajax({
+              url: app.people + dl.attr('data-id'),
+              method: 'patch',
+              contentType: "application/json",
+              data: JSON.stringify({
+                "person": {
+                  "name": dt.text(),
+                  "promoted": false,
+                }
+              }),
+              success: function(data) {
+                dl.removeClass("favorite");
+              },
+            });
+          }
+          else {
+            $.ajax({
+              url: app.people + dl.attr('data-id'),
+              method: 'patch',
+              contentType: "application/json",
+              data: JSON.stringify({
+                "person": {
+                  "name": dt.text(),
+                  "promoted": true,
+                }
+              }),
+              success: function(data) {
+                dl.addClass("favorite");
+              },
+            });
+          }
         }
       }
     });
 
-    // create down button
-    var downLink = this.buildLink({
-      contents: '<i class="fa fa-arrow-down fa-lg"></i>',
-      class: "down button tiny radius",
-      handler: function() {
-        // move item down one space
-        var nextDL = dl.next();
-        if (nextDL.length) {
-          dl.insertAfter(nextDL);
-          app.refreshRoster();
-          app.saveList();
-        }
-      }
-    });
+    // // create top button
+    // var topLink = this.buildLink({
+    //   contents: '<i class="fa fa-arrow-circle-up fa-lg"></i>',
+    //   class: "top button tiny radius",
+    //   handler: function() {
+    //     // move item to the top
+    //     dl.insertBefore(dl.siblings().first());
+    //     app.refreshRoster();
+    //     app.saveList();
+    //   }
+    // });
+    //
+    // // create up button
+    // var upLink = this.buildLink({
+    //   contents: '<i class="fa fa-arrow-up fa-lg"></i>',
+    //   class: "up button tiny radius",
+    //   handler: function() {
+    //     // move item up one space
+    //     var prevDL = dl.prev();
+    //     if (prevDL.length) {
+    //       dl.insertBefore(prevDL);
+    //       app.refreshRoster();
+    //       app.saveList();
+    //     }
+    //   }
+    // });
+    //
+    // // create down button
+    // var downLink = this.buildLink({
+    //   contents: '<i class="fa fa-arrow-down fa-lg"></i>',
+    //   class: "down button tiny radius",
+    //   handler: function() {
+    //     // move item down one space
+    //     var nextDL = dl.next();
+    //     if (nextDL.length) {
+    //       dl.insertAfter(nextDL);
+    //       app.refreshRoster();
+    //       app.saveList();
+    //     }
+    //   }
+    // });
 
     // put it all together
     editGroup.append(editLink, deleteLink, favoriteLink);
-    moveGroup.append(topLink, upLink, downLink);
-    ul.append(editGroup, moveGroup);
+    // moveGroup.append(topLink, upLink, downLink);
+    ul.append(editGroup);
     return dl.append(li.append(dt, dd.append(ul)));
   },
 
@@ -148,57 +210,65 @@ var app = {
       .click(options.handler);
   },
 
-  refreshRoster: function() {
-    $('a.top, a.up, a.down').removeClass("disabled");
-    $('dl:first-child a.top, \
-       dl:first-child a.up, \
-       dl:last-child a.down').addClass("disabled");
-  },
+  // refreshRoster: function() {
+  //   $('a.top, a.up, a.down').removeClass("disabled");
+  //   $('dl:first-child a.top, \
+  //      dl:first-child a.up, \
+  //      dl:last-child a.down').addClass("disabled");
+  // },
 
   // called on form submit
   addStudent: function(event) {
     event.preventDefault();
+    this.list.slideUp();
     var studentName = this.form.find('[name="studentName"]');
 
-    this.list.prepend(this.buildList(studentName.val(), false));
-    this.refreshRoster();
-    this.saveList();
-    studentName.val('').focus();
-  },
-
-  saveList: function() {
-    // simplest solution for me to save the list
-    // may be changed in the future to reduce time complexity
-    var studentList = [];
-    $.each(this.list.children(), function(i, dl) {
-      studentList.push({
-        name: $(dl).children().children().first().text(),
-        favorite: (function() {
-          if ($(dl).attr("class") === "favorite")
-            return true;
-          else return false;
-        })(),
-      });
+    var dl = this.buildList(studentName.val(), false);
+    this.list.append(dl);
+    $.ajax({
+      url: this.people,
+      method: 'post',
+      contentType: "application/json",
+      data: JSON.stringify({
+        "person": {
+          "name": studentName.val(),
+          "promoted": false,
+        }
+      }),
+      success: function(data) {
+        dl.attr('data-id', data.id);
+        studentName.val('').focus();
+        // this.refreshRoster();
+      },
     });
-
-    localStorage.setItem('roster', JSON.stringify(studentList));
+    this.list.slideDown();
   },
 
   loadRoster: function(event) {
+    this.list.slideUp();
     $.ajax({
       method: 'get',
-      url: 'https://mutant-school.herokuapp.com/api/v1/mutants',
+      url: app.people,
       success: function(roster) {
         $.each(roster, function(i, student) {
-          app.list.append(app.buildList(student.real_name, false));
+          app.list.append(app.buildList(student.name, student.promoted, student.id));
         });
       },
     });
+    this.list.slideDown();
   },
 
   clearRoster: function(event) {
-    this.list.html('');
-    this.saveList();
+    $.each(app.list.children(), function(i, dl) {
+      $.ajax({
+        url: app.people + dl.attr('data-id'),
+        method: 'delete',
+        success: function() {
+          dl.remove();
+          // app.refreshRoster();
+        },
+      });
+    });
   },
 };
 
